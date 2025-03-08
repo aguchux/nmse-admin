bf1a7c2d82f924a4e840e17e57b555e62a9b5267
Agu Stanley <agu.chux@golojan.co.uk>
2025-03-08 23:07:55 +0000

    switched to firebase

--- app/(root)/(auth)/auth/signup/page.tsx
+++ app/(root)/(auth)/auth/signup/page.tsx
@@ -3,8 +3,6 @@ import React from 'react'
 
 
 const SiginPage = () => {
-
-
     return (
         <SignupScreen />
     )
--- libs/firebase/server.ts
+++ libs/firebase/server.ts
@@ -6,8 +6,8 @@ if (!admin.apps.length) {
     // credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_ADMIN_CREDENTIALS as string) as admin.ServiceAccount)
     credential: admin.credential.cert({
       projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-      clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY && process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY && process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
     }),
 
   });
