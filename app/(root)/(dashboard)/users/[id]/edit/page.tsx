8a9cc00a65f00dce888512936f29246c7384bd38
Agu Stanley <agu.chux@golojan.co.uk>
2025-04-02 21:58:35 +0100

    All done

--- app/(root)/(dashboard)/users/[id]/edit/page.tsx
+++ app/(root)/(dashboard)/users/[id]/edit/page.tsx
@@ -24,7 +24,7 @@ const EditUserPage = () => {
     }
     return (
         <DivContainerFluid>
-            <EditUser user={user as IUser} />
+            <EditUser />
         </DivContainerFluid>
     )
 }
